dc8b74624f3853750a7f71a57e0af9ba0b2ce52a
jeff <leterip@me.com>
2012-01-14 16:48:23 -0500

    Pagination and pretty templates with errors!

--- adtest.go
+++ adtest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/zeebo/admin"
 	"launchpad.net/gobson/bson"
 	"launchpad.net/mgo"
@@ -30,16 +31,18 @@ type Post struct {
 func (p Post) GetTemplate() string {
 	return `<fieldset>
           <legend>Post</legend>
-          <div class="clearfix">
+          <div class="clearfix{{if .Errors.Title}} error{{end}}">
             <label for="Title">Title</label>
             <div class="input">
-              <input class="xlarge" id="Title" name="Title" size="30" type="text" />
+              <input class="xlarge{{if .Errors.Title}} error{{end}}" id="Title" name="Title" size="30" type="text" value="{{.Values.Title}}" />
+              {{if .Errors.Title}}<span class="help-inline">{{.Errors.Title.Error}}</span>{{end}}
             </div>
           </div><!-- /clearfix -->
-          <div class="clearfix">
+          <div class="clearfix{{if .Errors.Body}} error{{end}}">
             <label for="textarea">Body</label>
             <div class="input">
-              <textarea class="xxlarge" id="Body" name="Body" rows="10"></textarea>
+              <textarea class="xxlarge{{if .Errors.Body}} error{{end}}" id="Body" name="Body" rows="10">{{.Values.Body}}</textarea>
+              {{if .Errors.Body}}<span class="help-inline">{{.Errors.Body.Error}}</span>{{end}}
               <span class="help-block">
                 Markdown text
               </span>
@@ -50,7 +53,16 @@ func (p Post) GetTemplate() string {
           </div>
         </fieldset>`
 }
-func (p *Post) Validate() admin.ValidationErrors { return nil }
+func (p *Post) Validate() admin.ValidationErrors {
+	errs := make(admin.ValidationErrors)
+	if p.Title == "" {
+		errs["Title"] = errors.New("Required field.")
+	}
+	if p.Body == "" {
+		errs["Body"] = errors.New("Required field.")
+	}
+	return errs
+}
 
 type T2 struct {
 	ID bson.ObjectId `bson:"_id,omitempty"`
